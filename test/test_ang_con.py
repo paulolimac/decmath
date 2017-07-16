c4ef19645f18d99b632d80c6eaf9e10333124917
Evert Provoost <evert.provoost@gmail.com>
2017-07-16 20:49:21 +0200

    Added tests for ngular conversion functions (#2)

---
+++ test/test_ang_con.py
@@ -0,0 +1,18 @@
+# Import namespaces and helper functions from __init__.py
+from test import *
+
+def test_degrees():
+    assert abteq(dm.degrees(.5), m.degrees(.5))
+    assert abteq(dm.degrees(7.1), m.degrees(7.1))
+    assert abteq(dm.degrees(-.5), m.degrees(-.5))
+    assert abteq(dm.degrees(-7.1), m.degrees(-7.1))
+    assert dm.degrees(dm.pi) == 180
+    assert dm.degrees(dm.pi/2) == 90
+
+def test_radians():
+    assert abteq(dm.radians(.5), m.radians(.5))
+    assert abteq(dm.radians(7.1), m.radians(7.1))
+    assert abteq(dm.radians(-.5), m.radians(-.5))
+    assert abteq(dm.radians(-7.1), m.radians(-7.1))
+    assert dm.radians(180) == dm.pi
+    assert dm.radians(90) == dm.pi/2
